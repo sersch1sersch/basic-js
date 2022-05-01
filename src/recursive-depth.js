79ed47f5fcafe986cb371f5bcc8a87c03ba63088
sersch1sersch <sersch11031984@gmail.com>
2022-05-02 01:03:56 +0300

    ggg

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,21 +1,27 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * Implement class DepthCalculator with method calculateDepth
+ * Implement class itogCalculator with method calculateitog
  * that calculates deoth of nested array
  * 
  * @example
  * 
- * const depthCalc = new DepthCalculator();
- * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
- * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
- * depthCalc.calculateDepth([[[]]]) => 3
+ * const itogCalc = new itogCalculator();
+ * itogCalc.calculateitog([1, 2, 3, 4, 5]) => 1
+ * itogCalc.calculateitog([1, 2, 3, [4, 5]]) => 2
+ * itogCalc.calculateitog([[[]]]) => 3
  *
  */
+let hasNestedArray  = (arr)=> arr.some(x=>Array.isArray(x));
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  calculateDepth(arr ) {
+    let itog=1;
+    if(hasNestedArray(arr)){
+      let summ=arr.reduce((y,z)=>y.concat(z),[]);
+      return itog + this.calculateDepth(summ);
+    }
+    return itog
   }
 }
 
