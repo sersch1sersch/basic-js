075ee79aa7e674eb319d5f5c213f0b8ed96ee4bc
sersch1sersch <sersch11031984@gmail.com>
2022-05-02 00:47:36 +0300

    ggg

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,27 +4,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+ const chainMaker = {
+  itog:[],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.itog.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(item) {
+    this.itog.push(`( ${item} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+   if(this._strok(position)){
+     if(position<1 || position>this.getLength()){
+       this._pos()
+       throw new Error("Removing link position is out of range!");
+     }
+     let index =position - 1;
+     this.itog.splice(index ,1);
+     return this;
+   }
+   this._pos();
+   throw new Error("Position must be intager number!")
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.itog.reverse();
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let chain=this._getChain();
+    this._pos();
+    return chain;
+  },
+_strok(sum){
+return typeof sum === "number" && (sum ^ 0) === sum ;
+},
+_pos(){
+  this.itog.length = 0;
+},
+_getChain(){
+  return this.itog.join("~~");
+}
 };
 
 module.exports = {
